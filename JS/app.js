00ebc46853deb4b90d4a8d6ce9ef94911b73ffe1
david <dgorg18@freeuni.edu.ge>
2020-12-15 23:05:10 +0400

    Fixed authentication

---
+++ JS/app.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+  apiKey: "AIzaSyAW5tFnZjIXDGu6xqlMV2a1ggUW371d0Iw",
+  authDomain: "finalwebproject-5603e.firebaseapp.com",
+  projectId: "finalwebproject-5603e",
+  storageBucket: "finalwebproject-5603e.appspot.com",
+  messagingSenderId: "983510090217",
+  appId: "1:983510090217:web:13c7af5336187363e58b78",
+  measurementId: "G-0XCF1XZ8YJ"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
